08afec0ae298547ea11d1d978935c6a8995557b6
Jorge Guzmán <jorge.guzman.arciniegas@gmail.com>
2024-04-07 11:32:06 -0500

    Integración estilos modo informe de consumo

--- consumo/index.php
+++ consumo/index.php
@@ -1,127 +1,128 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $password = "samc2003";
-    $db = "pesoPlumaDb";
-    $con = new mysqli($host, $user, $password, $db);
+include_once '../API/db.php';
 
-    if(!empty($_POST)){
-        if(isset($_POST["fecha-inicial"]) && isset($_POST["fecha-final"])){
-        
-            $fechainicial = $_POST["fecha-inicial"];
-            $fechafinal = $_POST["fecha-final"];
-            $sql = "SELECT Consume.consume_date, Consume.consume_amount, Consume.consume_unit, Consume.consume_description, Ingredient.ingredient_name FROM Consume JOIN Ingredient ON Consume.ingredient_id = Ingredient.ingredient_id WHERE Consume.consume_date between '$fechainicial' and '$fechafinal';";
-        }
-    }else{
-        $sql = "SELECT Consume.consume_date, Consume.consume_amount, Consume.consume_unit, Consume.consume_description, Ingredient.ingredient_name FROM Consume JOIN Ingredient ON Consume.ingredient_id = Ingredient.ingredient_id";
+$db = new DB();
+$con = $db->conectarDB();
+
+if(!empty($_POST)){
+    if(isset($_POST["fecha-inicial"]) && isset($_POST["fecha-final"])){
+    
+        $fechainicial = $_POST["fecha-inicial"];
+        $fechafinal = $_POST["fecha-final"];
+        $sql = "SELECT Consume.consume_date, Consume.consume_amount, Consume.consume_unit, Ingredient.ingredient_name FROM Consume JOIN Ingredient ON Consume.ingredient_id = Ingredient.ingredient_id WHERE Consume.consume_date between '$fechainicial' and '$fechafinal';";
     }
+}else{
+    $sql = "SELECT Consume.consume_date, Consume.consume_amount, Consume.consume_unit, Ingredient.ingredient_name FROM Consume JOIN Ingredient ON Consume.ingredient_id = Ingredient.ingredient_id";
+}
 
-    $query = $con->query($sql); 
+$res = $con->query($sql); 
 
+$consumos_array = array();
+
+// Obtener cada fila de resultado y agregarla al array
+while ($row = $res->fetch_assoc()) {
+    $consumos_array[] = $row;
+}
+
+// Número de resultados por página
+$resultados_por_pagina = 6;
+
+// Número total de recetas
+$total_consumos = count($consumos_array);
+
+// Número total de páginas
+$total_paginas = ceil($total_consumos / $resultados_por_pagina);
+
+// Obtener la página actual
+if (!isset($_GET['pagina'])) {
+    $pagina_actual = 1;
+} else {
+    $pagina_actual = $_GET['pagina'];
+}
+
+// Calcular el índice de inicio para la consulta SQL
+$indice_inicio = ($pagina_actual - 1) * $resultados_por_pagina;
+
+// Obtener las recetas para la página actual
+$consumos_pagina = array_slice($consumos_array, $indice_inicio, $resultados_por_pagina);
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es" class="h-full">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php</title>
-    <!--  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.bootstrap5.css">
-    <script defer src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-    <script defer src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
-    <script defer src="https://cdn.datatables.net/2.0.2/js/dataTables.bootstrap5.js"></script>
-    <script defer src="./index.js"></script>
-    <!--  -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <style>
-        *{
-            box-sizing: border-box;
-            padding: 0%;
-            margin: 0;
-        }
-        .contenedor{
-            display: flex;
-            justify-content: center;
-            margin-top: 5vh;
-        }.contenedor2{
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }#tablaConsumo{
-            width: 900px;
-        }
-    
-    </style>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Modo Informe de Consumo</title>
 </head>
-<body>
-    <br>
-    <ul class="nav justify-content-center">
-        <li class="nav-item">
-            <a class="nav-link" href="./../pesoEnVivo/index.php">Medicion en vivo</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="./../receta/index.php">Recetas</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="./../index.php">Inicio</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="./../inventario/index.php">Inventario</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="./../consumo/index.php" aria-current="page">Informe de consumo</a>
-        </li>
-    </ul>
-    <hr>
-    <div class="contenedor2">
-        <h2 style="margin: 20px 0px;">Informe de consumo</h2>
-        <div class="row">
-            <table id="tablaConsumo" class="table table-bordered">
-                <thead>
-                  <tr>
-                    <th scope="col">Ingrediente</th>
-                    <th scope="col">Cantidad</th>
-                    <th scope="col">Unidad</th>
-                    <th scope="col">Fecha</th>
-                    <th scope="col">Descripcion</th>
-                  </tr>
-                </thead>
-                <tbody>
-                <?php while ($r=$query->fetch_array()):?>
-                    <tr>
-                        <td><?php echo $r["ingredient_name"];?></td>
-                        <td><?php echo $r["consume_amount"];?></td>
-                        <td><?php echo $r["consume_unit"];?></td>
-                        <td><?php echo $r["consume_date"];?></td>
-                        <td><?php echo $r["consume_description"];?></td>
-                    </tr>
-                <?php endwhile;?>
-                </tbody>
-              </table>
+<body class="h-full">
+    <header>
+        <div class="bg-blue-700 flex h-16 w-full shadow justify-around items-center">
+            <h1 class="basis-1/4 text-white text-xl font-bold text-center">
+                PesoPluma
+            </h1>
+            <nav class="flex-auto">
+                <ul class="flex justify-around">
+                    <li><a class="text-white text-md" href="../index.php">Inicio</a></li>
+                    <li><a class="text-white text-md" href="../receta/index.php">Receta</a></li>
+                    <li><a class="text-white text-md" href="../pesoEnVivo/index.php">Medición en Vivo</a></li>
+                    <li><a class="text-white text-md" href="../inventario/index.php">Inventario</a></li>
+                </ul>
+            </nav>
         </div>
-    </div>
-    <div class="contenedor2">
-        <h2 style="align-self: baseline;margin: 25px 140px;">Filtros</h2>
-        <form class="container" action="index.php" method="post">
-            <div class="form-floating mb-3">
-                <input type="date" class="form-control" id="fecha-inicial" name="fecha-inicial" placeholder="">
-                <label for="fecha-inicial">Fecha inicial</label>
+    </header>
+
+    <main>        
+        <article class="flex flex-row justify-beetween p-4">
+            
+            <div class="flex flex-col items-center flex-auto">
+                <h2 class="font-bold pb-1">Informe de Consumo</h2>
+                <table class="min-w-full divide-y divide-gray-200 border rounded">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider text-center">Ingrediente</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider text-center">Cantidad</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider text-center">Unidad</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider text-center">Fecha</th>
+                            <!-- Agrega más encabezados aquí si es necesario -->
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        <?php foreach ($consumos_pagina as $consumo): ?>
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-center"><?php echo $consumo['ingredient_name']; ?></td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-center"><?php echo $consumo["consume_amount"]; ?></td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-center"><?php echo $consumo['consume_unit']; ?></td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-center"><?php echo $consumo["consume_date"]; ?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+                <div class="flex justify-center mt-4">
+                    <?php for ($i = 1; $i <= $total_paginas; $i++): ?>
+                    <a href="?pagina=<?php echo $i; ?>" class="mx-1 px-3 py-1 bg-blue-700 text-white rounded hover:bg-blue-500"><?php echo $i; ?></a>
+                    <?php endfor; ?>
+                </div>
             </div>
-            <div class="form-floating mb-3">
-                <input type="date" class="form-control" id="fecha-final" name="fecha-final" placeholder="">
-                <label for="fecha-final">Fecha final</label>
+
+            <div class="flex flex-col basis-1/4 pl-4">
+                <h2 class="text-center font-bold pb-1">Filtros</h2>
+                <form action="index.php" method="post">
+                    <h3 class="text-lg border-b text-blue-700 border-blue-700 mb-3 font-bold w-full">
+                        Fecha inicial
+                    </h3>
+                    <input type="date" name="fecha-inicial" id="fecha-inicial" class="px-1 border rounded w-full">
+
+                    <h3 class="text-lg border-b text-blue-700 border-blue-700 mb-3 font-bold w-full mt-1">
+                        Fecha final
+                    </h3>
+                    <input type="date" id="fecha-final" name="fecha-final" class="border rounded w-full px-1" value="0">
+
+                    <button type="submit" class="block bg-blue-700 text-white py-1 px-3 rounded hover:bg-blue-transition-colors w-26 mt-3 shadow">Buscar</button>
+                </form>
             </div>
-            <button type="submit" class="btn btn-primary">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                </svg>
-                Buscar
-        </button>
-        </form>    
-    </div>
-    <br>
+
+        </article>
+    </main>
 </body>
 </html>
