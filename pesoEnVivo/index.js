459c9143274144e44abfb6275666c71ab6a49743
Jorge Guzmán <jorge.guzman.arciniegas@gmail.com>
2024-04-10 11:29:37 -0500

    Se añadió la validación de si hay suficiente cantidad del ingrediente en el inventario

--- pesoEnVivo/index.js
+++ pesoEnVivo/index.js
@@ -12,46 +12,54 @@ async function main() {
             const ingrediente = await obtenerIngredientePorIDJSON(ing.value);
             //console.log(ingrediente);
 
-            const lecturaActual = await obtenerDatoSensor();
-
-            let resultado = await conversionUnidades(lecturaActual, ingrediente, uni1.value, uni2.value);
-            let resultadoCD = await conversionUnidades(cantidadDeseada.value, ingrediente, uni1.value, uni2.value);
-
-            document.getElementById("dato_procesado").innerText = resultado;
-            document.getElementById("lectura_deseada").innerText = resultadoCD;
-            document.getElementById("unit1").innerText = uni2.value;
-            document.getElementById("unit2").innerText = uni2.value;
+            // Validar si hay suficiente cantidad del ingrediente
+            let cantidadDeseadaGramos = await conversionUnidades(cantidadDeseada.value, ingrediente, uni1.value, 'gramos');
             
-            // Creando variables globales
-            window.resultadoGlobal = resultado;
-            window.ingredienteIDGlobal = ing.value;
-            window.unidadIngredienteGlobal = uni2.value;
-            window.ingredienteGlobal = ingrediente;
-            window.resultadoGlobalGramos = resultado;
+            if (cantidadDeseadaGramos <= parseFloat(ingrediente['amount'])) {
+                const lecturaActual = await obtenerDatoSensor();
 
-            let botonConfirmar = document.getElementById("confirmar");
-            if ((resultado >= resultadoCD*0.98) && (resultado <= resultadoCD*1.02)) {
-                //console.log("Valor dentro del rango")
-                botonConfirmar.disabled = false;
-                botonConfirmar.classList.remove('bg-gray-500');
-                botonConfirmar.classList.add('bg-blue-700');
-                document.getElementById("accion_requerida").innerText = "Ninguna";
-            } else {
-                //console.log("Valor fuera del rango");
-                botonConfirmar.disabled = true;
-                botonConfirmar.classList.remove('bg-blue-700');
-                botonConfirmar.classList.add('bg-gray-500');
-
-                // Alarmas
-                if (resultado < resultadoCD*0.98) {
-                    console.log("Agrege más ingrediente");
-                    document.getElementById("accion_requerida").innerText = "Agrege más ingrediente";
-                    beep();
-                } else if (resultado > resultadoCD*1.02) {
-                    console.log("Retire cantidad del ingrediente");
-                    document.getElementById("accion_requerida").innerText = "Retire cantidad del ingrediente";
-                    beep();
+                let resultado = await conversionUnidades(lecturaActual, ingrediente, uni1.value, uni2.value);
+                let resultadoCD = await conversionUnidades(cantidadDeseada.value, ingrediente, uni1.value, uni2.value);
+    
+                document.getElementById("dato_procesado").innerText = resultado;
+                document.getElementById("lectura_deseada").innerText = resultadoCD;
+                document.getElementById("unit1").innerText = uni2.value;
+                document.getElementById("unit2").innerText = uni2.value;
+                
+                // Creando variables globales
+                window.resultadoGlobal = resultado;
+                window.ingredienteIDGlobal = ing.value;
+                window.unidadIngredienteGlobal = uni2.value;
+                window.ingredienteGlobal = ingrediente;
+                window.resultadoGlobalGramos = resultado;
+    
+                let botonConfirmar = document.getElementById("confirmar");
+                if ((resultado >= resultadoCD*0.98) && (resultado <= resultadoCD*1.02)) {
+                    //console.log("Valor dentro del rango")
+                    botonConfirmar.disabled = false;
+                    botonConfirmar.classList.remove('bg-gray-500');
+                    botonConfirmar.classList.add('bg-blue-700');
+                    document.getElementById("accion_requerida").innerText = "Ninguna";
+                } else {
+                    //console.log("Valor fuera del rango");
+                    botonConfirmar.disabled = true;
+                    botonConfirmar.classList.remove('bg-blue-700');
+                    botonConfirmar.classList.add('bg-gray-500');
+    
+                    // Alarmas
+                    if (resultado < resultadoCD*0.98) {
+                        console.log("Agrege más ingrediente");
+                        document.getElementById("accion_requerida").innerText = "Agrege más ingrediente";
+                        beep();
+                    } else if (resultado > resultadoCD*1.02) {
+                        console.log("Retire cantidad del ingrediente");
+                        document.getElementById("accion_requerida").innerText = "Retire cantidad del ingrediente";
+                        beep();
+                    }
                 }
+            } else {
+                alert("No hay suficiente cantidad del ingrediente.");
+                location.reload();
             }
         }
     } catch (error) {
